dfa1bcbfda74b1540f9e364ac388f85f40328ac8
Arnaud <arno969@gmail.com>
2017-04-27 12:23:08 +0200

    Class Post

---
+++ src/main/java/HPP_PROJECT/Post.java
@@ -0,0 +1,15 @@
+package HPP_PROJECT;
+
+public class Post {
+	 int ts;
+	 private int post_id;
+	 private int user_id;
+	 private String user;
+	 
+	 
+	 public Post(int time)
+	 {
+		 ts = time;
+	 }
+	 
+}
