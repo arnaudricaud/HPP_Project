31bdf8ba8f7317a32ba12238899e281d9b33e201
Arthurgaildry <arthur2_917@hotmail.fr>
2017-04-27 12:30:10 +0200

    comment class

---
+++ src/main/java/HPP_PROJECT/Comment.java
@@ -0,0 +1,16 @@
+package HPP_PROJECT;
+
+public class Comment {
+	
+
+		
+		private int ts; //is the comment's timestamp
+		private int comment_id; // is the unique id of the comment
+		private int user_id;// 	is the unique id of the user
+		private String comment; // is a string containing the actual comment
+		private String user; // 	is a string containing the actual user name
+		private int comment_replied; // is the id of the comment being replied to (-1 if the tuple is a reply to a post)
+		private int post_commented; // is the id of the post being commented (-1 if the tuple is a reply to a comment)
+
+
+}
