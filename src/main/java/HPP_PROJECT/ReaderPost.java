d9de1bed0ae0606714578faa0fa7a476b70ce4a9
Arthurgaildry <arthur2_917@hotmail.fr>
2017-05-09 13:59:37 +0200

    Readerpost
    split reader

--- src/main/java/HPP_PROJECT/ReaderPost.java
+++ src/main/java/HPP_PROJECT/ReaderPost.java
@@ -6,6 +6,8 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 public class ReaderPost {
 
@@ -20,15 +22,16 @@ public class ReaderPost {
     	}
     	
     	return split(line);
-
     }
     
     public static Post split(String line)
     {
     	
     	String[] list = line.split("\\|");
+    	DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-ddTHH:mm:ss");
+    	DateTime dt = formatter.parseDateTime(list[0]);
     	
-    	return new Post()
+    	return new Post(dt, Integer.parseInt(list[1]), Integer.parseInt(list[2]), list[4], 0)
     }
     
     
