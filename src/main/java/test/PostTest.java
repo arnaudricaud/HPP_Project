c348ac5c7280f21a4c48c027f3811fcb09ea6838
Arnaud <arno969@gmail.com>
2017-05-09 14:21:23 +0200

    Fin Test "ReaderPost"

--- src/main/java/test/PostTest.java
+++ src/main/java/test/PostTest.java
@@ -12,7 +12,7 @@ public class PostTest {
 	@Test
 	public void ReadPostTest() {
 		//TODO Remplir format DateTime
-		Post expected = new Post(DateTime time, 1039993, 3981, "Lei Liu", 10);
+		Post expected = new Post(new DateTime("2010-02-01T05:12:32.921+0000") ,1039993, 3981, "Lei Liu", 10);
 		Post actual = Post.ReadPost("ressources/test/ReadPost/postsTest1.dat");
 		assertEquals(true, expected.equals(actual));
 	}
