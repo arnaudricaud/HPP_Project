6b1dcb2de3b070a82b60808bf07c6c5783cd7d03
Arnaud <arno969@gmail.com>
2017-05-10 10:26:21 +0200

    Update score et tests

--- src/main/java/test/CommentTest.java
+++ src/main/java/test/CommentTest.java
@@ -11,23 +11,19 @@ import HPP_PROJECT.ReaderComment;
 
 public class CommentTest {
 
-	@Before
-	public void initReader(){
-		ReaderComment reader = new ReaderComment("ressources/test/BasicTest/commentsTest1.dat");
-	}
+	private ReaderComment reader = ReaderComment.getInstance("ressources/test/BasicTest/commentsTest1.dat");
 	
 	@Test
 	public void readCommentTest() {
 		Comment expected = new Comment(new DateTime("2010-02-09T04:05:20.777+0000") ,529590, 2886, "Baoping Wu", -1, 529360);
-		Comment actual = ReaderComment.ReadComment();
+		Comment actual = reader.readNextComment();
 		assertEquals(true, expected.equals(actual));
 	}
 	
 	@Test
 	public void read2ndCommentTest() {
 		Comment expected = new Comment(new DateTime("2010-02-09T04:20:53.281+0000") ,529589, 2886, "Baoping Wu", -1, 529360);
-		Comment actual = ReaderComment.ReadComment();
-		actual = ReaderComment.ReadComment();
+		Comment actual = reader.readNextComment();
 		assertEquals(true, expected.equals(actual));
 	}
 
--- src/main/java/test/PostTest.java
+++ src/main/java/test/PostTest.java
@@ -10,23 +10,20 @@ import org.junit.Test;
 
 public class PostTest {
 
-	@Before
-	public void initReader(){
-	    ReaderPost reader = new ReaderPost("ressources/test/BasicTest/postsTest1.dat");
-	}
-	
+	private ReaderPost reader = new ReaderPost("ressources/test/BasicTest/postsTest1.dat");
+
 	@Test
 	public void readPostTest() {
 		Post expected = new Post(new DateTime("2010-02-01T05:12:32.921+0000") ,1039993, 3981, "Lei Liu");
-		Post actual = ReaderPost.ReadPost();
+		Post actual = reader.readPost();
 		assertEquals(true, expected.equals(actual));
 	}
 	
 	@Test
 	public void read2ndPostTest() {
 		Post expected = new Post(new DateTime("2010-02-02T19:53:43.226+0000") ,299101, 4661, "Michael Wang");
-		Post actual = ReaderPost.ReadPost();
-		actual = ReaderPost.ReadPost();
+		Post actual = reader.readPost();
+		actual = reader.readPost();
 		assertEquals(true, expected.equals(actual));
 	}
 
