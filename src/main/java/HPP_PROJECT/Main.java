abe11cb4403e4ebd69eb54ac18b1dcaaccc503eb
Arnaud <arno969@gmail.com>
2017-05-10 14:51:25 +0200

    Correction écriture

--- src/main/java/HPP_PROJECT/Main.java
+++ src/main/java/HPP_PROJECT/Main.java
@@ -1,5 +1,6 @@
 package HPP_PROJECT;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -15,13 +16,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		ArrayList<Post> expected = new ArrayList<Post>();
-		//expected.add(new Post(new DateTime("2010-02-01T05:12:32.921+0000") ,1039993, 3981, "Lei Liu"));
-		//expected.add(new Post(new DateTime("2010-02-02T19:53:43.226+0000") ,299101, 4661, "Michael Wang"));
-		expected.add(new Post(new DateTime("2010-02-03T19:53:43.226+0000") ,255120, 4661, "Michael Wang"));
+		// expected.add(new Post(new DateTime("2010-02-01T05:12:32.921+0000")
+		// ,1039993, 3981, "Lei Liu"));
+		// expected.add(new Post(new DateTime("2010-02-02T19:53:43.226+0000")
+		// ,299101, 4661, "Michael Wang"));
+		expected.add(new Post(new DateTime("2010-02-03T19:53:43.226+0000"), 255120, 4661, "Michael Wang"));
 		expected.get(0).setScoreTotal(10);
-		//expected.get(1).setScoreTotal(20);
-		//expected.get(2).setScoreTotal(30);
+		// expected.get(1).setScoreTotal(20);
+		// expected.get(2).setScoreTotal(30);
+		clearHistoriqueFile();
 		writeTop3(expected, new DateTime("2010-02-03T19:53:43.226+0000"));
+		writeTop3(expected, new DateTime("2010-02-03T20:53:43.226+0000"));
 	}
 
 	public static DateTime nextTick(Post p, Comment c) {
@@ -61,26 +66,40 @@ public class Main {
 		});
 	}
 
+	
 	public static void writeTop3(ArrayList<Post> tabPost, DateTime tk) {
 		sortPost(tabPost);
 		try {
-			PrintWriter writer = new PrintWriter("historique.txt", "UTF-8");
-			writer.print("<" + tk + ",");
+			FileWriter writer = new FileWriter("export/historique.txt", true);
+			writer.write("<" + tk + ",");
 			for (int i = 0; i < 2; i++) {
 				if (i < tabPost.size()) {
-					writer.println(tabPost.get(i).getPost_id() + "," + tabPost.get(i).getUser() + "," + tabPost.get(i).getScoreTotal() + ",");
+					writer.write(tabPost.get(i).getPost_id() + "," + tabPost.get(i).getUser() + ","
+							+ tabPost.get(i).getScoreTotal() + ",\r\n");
 				} else {
-					writer.println("-,-,-,-,");
+					writer.write("-,-,-,-,\r\n");
 				}
 			}
-			if (tabPost.size() >=3) {
-				writer.println(tabPost.get(2).getPost_id() + "," + tabPost.get(2).getUser() + "," + tabPost.get(2).getScoreTotal() + ">");
+			if (tabPost.size() >= 3) {
+				writer.write(tabPost.get(2).getPost_id() + "," + tabPost.get(2).getUser() + ","
+						+ tabPost.get(2).getScoreTotal() + ">\r\n");
 			} else {
-				writer.println("-,-,-,->");
+				writer.write("-,-,-,->\r\n");
 			}
 			writer.close();
 		} catch (IOException e) {
 			// do something
+		}
+
+	}
+
+	public static void clearHistoriqueFile() {
+		try {
+			PrintWriter writer = new PrintWriter("export/historique.txt");
+			writer.print("");
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 	}
@@ -92,24 +111,22 @@ public class Main {
 			}
 		}
 	}
-	
-	public static void calculScore(){
-		//appel fonction
-		for(int i=0;i<tabPost.size();i++){
-			for(int j=0;j<tabComment.size();j++){
-				
-				if(tabComment.get(j).getPost_commented()==tabPost.get(i).getPost_id()){
-					tabPost.get(i).setScoreTotal(tabPost.get(i).getScoreTotal() +tabComment.get(j).getScore());
-					
-					
+
+	public static void calculScore() {
+		// appel fonction
+		for (int i = 0; i < tabPost.size(); i++) {
+			for (int j = 0; j < tabComment.size(); j++) {
+
+				if (tabComment.get(j).getPost_commented() == tabPost.get(i).getPost_id()) {
+					tabPost.get(i).setScoreTotal(tabPost.get(i).getScoreTotal() + tabComment.get(j).getScore());
+
 				}
-				
-				
+
 			}
 			supression();
-			
+
 		}
-		
+
 	}
 
 }
