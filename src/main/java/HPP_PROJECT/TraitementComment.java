ce1b742259f2ccf4f266eaef232e56a7d5e5e788
Arnaud <arno969@gmail.com>
2017-05-10 12:29:36 +0200

    Ajout de test et Modifications des traitement des commentaires

--- src/main/java/HPP_PROJECT/TraitementComment.java
+++ src/main/java/HPP_PROJECT/TraitementComment.java
@@ -6,12 +6,37 @@ import org.joda.time.DateTime;
 
 public class TraitementComment {
 
-	public static void traitement(DateTime tk, ArrayList<Comment> tabComment, ReaderComment rdComment) {
+	public static void traitement(DateTime tk, ArrayList<Comment> tabComment, ArrayList<Post> tabPost, ReaderComment rdComment) {
 
+		Boolean truePost;
+		
+		
 		Comment cmt = rdComment.getCurrentComment();
 
 		while (cmt.getTs().equals(tk)) {
-			tabComment.add(cmt);
+			truePost = false;
+			if (cmt.getPost_commented() == -1) {
+				for (int i = 0; i < tabComment.size(); i++) {
+					if (tabComment.get(i).getComment_id() == cmt.getComment_replied()) {
+						cmt.setPost_commented(tabComment.get(i).getPost_commented());
+						truePost = true;
+						break;
+					}
+				}
+			}
+			
+			for (int j = 0; j < tabPost.size(); j++) {
+				if (tabPost.get(j).getPost_id() == cmt.getPost_commented()) {
+					truePost = true;
+					break;
+				}
+			}
+			
+			if (truePost == true)
+			{
+				tabComment.add(cmt);
+			}
+			
 			cmt = rdComment.readNextComment();
 		}
 	}
--- src/main/java/test/CommentTest.java
+++ src/main/java/test/CommentTest.java
@@ -8,6 +8,7 @@ import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 import HPP_PROJECT.Comment;
+import HPP_PROJECT.Post;
 import HPP_PROJECT.ReaderComment;
 import HPP_PROJECT.TraitementComment;
 
@@ -22,7 +23,7 @@ public class CommentTest {
 	@Test
 	public void readCommentTest() {
 		ReaderComment reader = ReaderComment.getInstance("ressources/test/BasicTest/commentsTest1.dat");
-		Comment expected = new Comment(new DateTime("2010-02-09T04:05:20.777+0000") ,529590, 2886, "Baoping Wu", -1, 529360);
+		Comment expected = new Comment(new DateTime("2010-02-09T04:05:20.777+0000") ,529590, 2886, "Baoping Wu", -1, 1039993);
 		Comment actual = reader.readNextComment();
 		assertEquals(true, expected.equals(actual));
 	}
@@ -30,7 +31,7 @@ public class CommentTest {
 	@Test
 	public void readCommentTest2() {
 		ReaderComment reader = ReaderComment.getInstance("ressources/test/BasicTest/commentsTest1.dat");
-		Comment expected = new Comment(new DateTime("2010-02-09T04:20:53.281+0000") ,529589, 2886, "Baoping Wu", -1, 529360);
+		Comment expected = new Comment(new DateTime("2010-02-09T04:20:53.281+0000") ,529589, 2886, "Baoping Wu", -1, 299101);
 		Comment actual = reader.readNextComment();
 		actual = reader.readNextComment();
 		assertEquals(true, expected.equals(actual));
@@ -40,12 +41,45 @@ public class CommentTest {
 	public void readMultipleCommentTest() {
 		ReaderComment reader = ReaderComment.getInstance("ressources/test/BasicTest/commentsTest1.dat");
 		reader.readNextComment();
+		ArrayList<Post> tabPost = new ArrayList<Post>();
+		tabPost.add(new Post(new DateTime("2010-02-01T05:12:32.921+0000") ,1039993, 3981, "Lei Liu"));
+		tabPost.add(new Post(new DateTime("2010-02-02T19:53:43.226+0000") ,299101, 4661, "Michael Wang"));
+		
 		ArrayList<Comment> expected = new ArrayList<Comment>();
 		ArrayList<Comment> actual = new ArrayList<Comment>();
-		expected.add(new Comment(new DateTime("2010-02-09T04:05:20.777+0000") ,529590, 2886, "Baoping Wu", -1, 529360));
-		expected.add(new Comment(new DateTime("2010-02-09T04:20:53.281+0000") ,529589, 2886, "Baoping Wu", -1, 529360));
-		TraitementComment.traitement(new DateTime("2010-02-09T04:05:20.777+0000"), actual, reader);
-		TraitementComment.traitement(new DateTime("2010-02-09T04:20:53.281+0000"), actual, reader);
+		expected.add(new Comment(new DateTime("2010-02-09T04:05:20.777+0000") ,529590, 2886, "Baoping Wu", -1, 1039993));
+		expected.add(new Comment(new DateTime("2010-02-09T04:20:53.281+0000") ,529589, 2886, "Baoping Wu", -1, 299101));
+		TraitementComment.traitement(new DateTime("2010-02-09T04:05:20.777+0000"), actual, tabPost, reader);
+		TraitementComment.traitement(new DateTime("2010-02-09T04:20:53.281+0000"), actual, tabPost, reader);
+
+		Boolean test;
+		if ((actual.get(0).equals(expected.get(0))) && (actual.get(1).equals(expected.get(1)))){
+			test = true;
+		} else{
+			test = false;
+		}
+
+		assertEquals(true, test);
+	}
+	
+	@Test
+	public void readMultipleCommentWithFalseTest() {
+		ReaderComment reader = ReaderComment.getInstance("ressources/test/BasicTest/commentsTest2.dat");
+		reader.readNextComment();
+		ArrayList<Post> tabPost = new ArrayList<Post>();
+		tabPost.add(new Post(new DateTime("2010-02-01T05:12:32.921+0000") ,1039993, 3981, "Lei Liu"));
+		tabPost.add(new Post(new DateTime("2010-02-02T19:53:43.226+0000") ,299101, 4661, "Michael Wang"));
+		
+		
+		ArrayList<Comment> expected = new ArrayList<Comment>();
+		ArrayList<Comment> actual = new ArrayList<Comment>();
+		expected.add(new Comment(new DateTime("2010-02-09T04:05:20.777+0000") ,529590, 2886, "Baoping Wu", -1, 1039993));
+		expected.add(new Comment(new DateTime("2010-02-09T04:20:53.281+0000") ,529589, 2886, "Baoping Wu", -1, 299101));
+		expected.add(new Comment(new DateTime("2010-02-09T06:08:38.206+0000") ,529594, 3633, "Jun Wu", 529590, 1039993));
+		TraitementComment.traitement(new DateTime("2010-02-09T04:05:20.777+0000"), actual, tabPost, reader);
+		TraitementComment.traitement(new DateTime("2010-02-09T04:20:53.281+0000"), actual, tabPost, reader);
+		TraitementComment.traitement(new DateTime("2010-02-09T05:19:19.802+0000"), actual, tabPost, reader);
+		TraitementComment.traitement(new DateTime("2010-02-09T06:08:38.206+0000"), actual, tabPost, reader);
 		
 		Boolean test;
 		if ((actual.get(0).equals(expected.get(0))) && (actual.get(1).equals(expected.get(1)))){
@@ -56,6 +90,7 @@ public class CommentTest {
 
 		assertEquals(true, test);
 	}
+	
 	
 
 }
