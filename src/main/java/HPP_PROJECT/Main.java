cc29d7c86d9c92f3573a7d4d8f14bf88c4ab3f47
oneirics <oneiricsg@gmail.com>
2017-05-10 16:32:52 +0200

    Main
    Function assembling

--- src/main/java/HPP_PROJECT/Main.java
+++ src/main/java/HPP_PROJECT/Main.java
@@ -13,26 +13,45 @@ public class Main {
 	static ArrayList<Comment> tabComment = new ArrayList<Comment>();
 
 	public static void main(String[] args) {
-		ArrayList<Post> expected = new ArrayList<Post>();
-		// expected.add(new Post(new DateTime("2010-02-01T05:12:32.921+0000")
-		// ,1039993, 3981, "Lei Liu"));
-		// expected.add(new Post(new DateTime("2010-02-02T19:53:43.226+0000")
-		// ,299101, 4661, "Michael Wang"));
-		expected.add(new Post(new DateTime("2010-02-03T19:53:43.226+0000"), 255120, 4661, "Michael Wang"));
-		expected.get(0).setScoreTotal(10);
-		// expected.get(1).setScoreTotal(20);
-		// expected.get(2).setScoreTotal(30);
-		clearHistoriqueFile();
-		writeTop3(expected, new DateTime("2010-02-03T19:53:43.226+0000"));
-		writeTop3(expected, new DateTime("2010-02-03T20:53:43.226+0000"));
+		ReaderPost rdPost = ReaderPost.getInstance("ressources/data/posts.dat");
+		ReaderComment rdComment = ReaderComment.getInstance("ressources/data/comment.dat");
+		
+		rdPost.readNextPost();
+		rdComment.readNextComment();
+		
+		DateTime tk = nextTick(rdPost.getCurrentPost(), rdComment.getCurrentComment());
+		while(tk  != null)
+		{
+			TraitementPost.traitement(tk, tabPost, rdPost);
+			TraitementComment.traitement(tk, tabComment, tabPost, rdComment);
+			for(int i = 0; i < tabComment.size(); i++)
+				updateComment(tabComment.get(i), tk);
+			
+			for (int i = 0; i < tabPost.size(); i++)
+			{
+				updatePost(tabPost.get(i), tk);
+				calculScore(tk);			
+			}
+			suppression();
+			sortPost(tabPost);
+			writeTop3(tabPost, tk);
+			nextTick(rdPost.getCurrentPost(), rdComment.getCurrentComment());
+			
+		}
+		
 	}
 
 	public static DateTime nextTick(Post p, Comment c) {
-		long diff = p.getTs().getMillis() - c.getTs().getMillis();
-		if (diff >= 0) {
-			return c.getTs();
-		} else {
-			return p.getTs();
+		if(p.getUser_id() == -1 && c.getComment_id() == -1)
+			return null;
+		else
+		{
+			long diff = p.getTs().getMillis() - c.getTs().getMillis();
+			if (diff >= 0) {
+				return c.getTs();
+			} else {
+				return p.getTs();
+			}
 		}
 	}
 
@@ -102,10 +121,24 @@ public class Main {
 
 	}
 
-	public static void supression() {
-		for (int i = 0; i < tabPost.size(); i++) {
-			if (tabPost.get(i).getScorePost() == 0) {
+	public static void suppression() 
+	{
+		for (int i = 0; i < tabPost.size(); i++)
+		{
+			if (tabPost.get(i).getScorePost() == 0) 
+			{
+				int idPost = tabPost.get(i).getPost_id();
 				tabPost.remove(i);
+				i--;// Le remove reagence les index
+				for (int j = 0; j < tabComment.size(); j++) 
+				{
+					 if(tabComment.get(j).getPost_commented() == idPost)
+					 {
+						tabComment.remove(j);
+					 	j--;
+					 }
+				}
+				
 			}
 		}
 	}
@@ -120,10 +153,6 @@ public class Main {
 				{
 					tabPost.get(i).setScoreTotal(tabPost.get(i).getScoreTotal() + tabComment.get(j).getScore());				
 				}						
-			}
-			if(tabPost.get(i).getScoreTotal() == 0)
-			{
-				tabPost.remove(i);
 			}
 		}
 		
